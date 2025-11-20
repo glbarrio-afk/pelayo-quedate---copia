84f1e363dbb0c0edb4dbef87f2f910741cbf03c9
glbarrio-afk <glbarrio@lsmaravillas.com>
2025-11-20 15:55:41 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,1 @@
-input.onButtonPressed(Button.A, function () {
-    wuKong.setAllMotor(-100, -100)
-})
-input.onButtonPressed(Button.AB, function () {
-    wuKong.stopAllMotor()
-})
-input.onButtonPressed(Button.B, function () {
-    wuKong.setAllMotor(100, 100)
-})
+
